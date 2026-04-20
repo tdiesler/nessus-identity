2c9dacba34ec040c3f8ce45434da7b3eefe27f30
Thomas Diesler <tdiesler@proton.me>
2026-04-20 11:23:34 +0200

    [OID4VP] Ability to run OpenID conformance suite for Verifier
    closes #378

--- scripts/openid-conformance.sh
+++ scripts/openid-conformance.sh
@@ -10,13 +10,24 @@ SCRIPT_DIR=$(realpath "$(dirname "$0")")
 CONFORMANCE_DIR=$(realpath "${SCRIPT_DIR}/../../conformance-suite")
 CONFORMANCE_SCRIPTS_DIR="${CONFORMANCE_DIR}/scripts"
 
-DEFAULT_CONFIG_FILE="${SCRIPT_DIR}/config/keycloak-openid-config.json"
-DEFAULT_FAILURES_FILE="${SCRIPT_DIR}/config/keycloak-openid-failures.json"
-DEFAULT_SKIPS_FILE="${SCRIPT_DIR}/config/keycloak-openid-skips.json"
-DEFAULT_FILTERS_FILE="${SCRIPT_DIR}/config/keycloak-openid-filters.json"
-
-PLAN_NAME="oid4vci-1_0-issuer-haip-test-plan"
-DEFAULT_VARIANTS="[vci_authorization_code_flow_variant=wallet_initiated][credential_format=sd_jwt_vc]"
+SCRIPT_CONFIG='{
+  "issuer": {
+    "plan_name": "oid4vci-1_0-issuer-haip-test-plan",
+    "variants": "[credential_format=sd_jwt_vc][vci_authorization_code_flow_variant=wallet_initiated]",
+    "config_file": "keycloak-openid-issuer-config.json",
+    "failures_file": "keycloak-openid-issuer-failures.json",
+    "filters_file": "keycloak-openid-issuer-filters.json",
+    "skips_file": "keycloak-openid-issuer-skips.json"
+  },
+  "verifier": {
+    "plan_name": "oid4vp-1final-verifier-haip-test-plan",
+    "variants": "[credential_format=sd_jwt_vc][response_mode=direct_post.jwt]",
+    "config_file": "keycloak-openid-verifier-config.json",
+    "failures_file": "keycloak-openid-verifier-failures.json",
+    "filters_file": "keycloak-openid-verifier-filters.json",
+    "skips_file": "keycloak-openid-verifier-skips.json"
+  }
+}'
 
 KC_REALM="oid4vci"
 
@@ -46,8 +57,8 @@ esac
 init_opts() {
   opt_clean=false
   opt_show_help=false
-  opt_show_modules=false
-  opt_run_all=false
+  opt_show_role=""
+  opt_run_role=""
   opt_run_config=""
   opt_run_module=""
   opt_run_profile=""
@@ -56,19 +67,24 @@ init_opts() {
 init_opts
 
 show_help() {
-  echo "usage: $0 [--clean] [--help] [--run-all] [--run-test module] [--run-profile name] [--show-modules]"
+  echo "usage: $0 [--clean] [--help] [--show-modules role] [--run role] [--run-module role module] [--run-profile name]"
   echo ""
   echo "  --clean           Cleans existing test plans from the database"
-  echo "  --run-all         Run the default profile and all other"
-  echo "  --run-test        Run a single test module"
+  echo "  --run             Run all profiles for a given role"
+  echo "  --run-module      Run a single test module"
   echo "  --run-profile     Run the given test profile"
-  echo "  --show-modules    Show effective test modules"
+  echo "  --show-modules    Show effective test modules for a given role"
+  echo ""
+  echo "  Roles"
+  echo "    - issuer        Issuer modules"
+  echo "    - verifier      Verifier modules"
   echo ""
   echo "  Profiles"
-  echo "    - [0|default]                             Run the default profile"
-  echo "    - [1|oid4vci-attestation-proof]           Uses proof type 'attestation' instead of 'jwt'"
-  echo "    - [2|oid4vci-credential-encryption]       Variant [vci_credential_encryption=encrypted]"
-  echo "    - [3|fapi2-user-rejects-authentication]   Use rejects consent during authentication"
+  echo "    - [1|issuer]                              Run the default issuer profile"
+  echo "    - [2|verifier]                            Run the default verifier profile"
+  echo "    - [3|oid4vci-attestation-proof]           Uses proof type 'attestation' instead of 'jwt'"
+  echo "    - [4|oid4vci-credential-encryption]       Variant [vci_credential_encryption=encrypted]"
+  echo "    - [5|fapi2-user-rejects-authentication]   Use rejects consent during authentication"
   echo ""
 }
 
@@ -82,18 +98,40 @@ while [[ $# -gt 0 ]]; do
       break
       ;;
     --show-modules)
-      opt_show_modules=true
-      break
+      if [[ -z "${2-}" || "${2-}" == --* ]]; then
+        echo "Role name required (e.g. --show-modules issuer)" >&2
+        exit 1
+      fi
+      opt_show_role="$2"
+      shift
       ;;
-    --run-all)
-      opt_run_all=true
+    --run)
+      if [[ -z "${2-}" || "${2-}" == --* ]]; then
+        echo "Role name required (e.g. --run issuer)" >&2
+        exit 1
+      fi
+      opt_run_role="$2"
+      shift
       ;;
-    --run-test)
-      if [[ -n "${2-}" && "${2-}" != --* ]]; then
-        opt_run_module="$2"
-        shift
+    --run-module)
+      if [[ -z "${2-}" || "${2-}" == --* ]]; then
+        echo "Role name required (e.g. --run-module issuer)" >&2
+        exit 1
+      fi
+      opt_run_role="$2"
+      shift
+      if [[ -z "${2-}" || "${2-}" == --* ]]; then
+        case "${opt_run_role}" in
+          issuer)
+            opt_run_module="oid4vci-1_0-issuer-happy-flow"
+            ;;
+          verifier)
+            opt_run_module="oid4vp-1final-verifier-happy-flow"
+            ;;
+        esac
       else
-        opt_run_module="oid4vci-1_0-issuer-happy-flow"
+        opt_run_module="$3"
+        shift
       fi
       if [[ -n "${2-}" && "${2-}" != --* ]]; then
         opt_run_config="$2"
@@ -101,12 +139,12 @@ while [[ $# -gt 0 ]]; do
       fi
       ;;
     --run-profile)
-      if [[ -n "${2-}" && "${2-}" != --* ]]; then
-        opt_run_profile="$2"
-        shift
-      else
-        opt_run_profile="default"
+      if [[ -z "${2-}" || "${2-}" == --* ]]; then
+        echo "Profile name/index is required (e.g. --run-profile default)" >&2
+        exit 1
       fi
+      opt_run_profile="$2"
+      shift
       ;;
     *)
       echo "Unknown option: $1";
@@ -137,28 +175,40 @@ _deactivate_venv() {
 }
 
 _get_effective_modules() {
-  filtered_modules=$(_get_filtered_modules)
+  local role="$1"
+  plan=$(_get_plan_name "${role}")
+  filtered_modules=$(_get_filtered_modules "${role}")
   while IFS= read -r mod; do
     if ! printf "%s\n" "${filtered_modules}" | grep -F -x -q "$mod"; then
       printf "%s\n" "$mod"
     fi
-  done <<< "$(_get_modules)"
+  done <<< "$(_get_modules "${plan}")"
 }
 
 _get_filtered_modules() {
-  jq -r '.[]."test-name"' "${DEFAULT_FILTERS_FILE}"
+  local role="$1"
+  filters_file=$(jq -r ".${role}.filters_file" <<< "${SCRIPT_CONFIG}")
+  jq -r '.[]."test-name"' "${SCRIPT_DIR}/config/${filters_file}"
 }
 
 _get_modules() {
-  curl -ks "${CONFORMANCE_SERVER}/api/plan/info/${PLAN_NAME}" | jq -r '.modules[].testModule'
+  local plan="$1"
+  curl -ks "${CONFORMANCE_SERVER}/api/plan/info/${plan}" | jq -r '.modules[].testModule'
+}
+
+_get_plan_name() {
+  local role="$1"
+  jq -r ".${role}.plan_name" <<< "${SCRIPT_CONFIG}"
 }
 
 _run_test_modules() {
-  local variants="$1"
-  local modules="$2"
-  local failures="$3"
-  local skips="$4"
-  local config="$5"
+  local role="$1"
+  local plan="$2"
+  local variants="$3"
+  local modules="$4"
+  local failures="$5"
+  local skips="$6"
+  local config="$7"
 
   local cmd_args=(--no-parallel --verbose)
 
@@ -170,8 +220,8 @@ _run_test_modules() {
     cmd_args+=(--expected-skips-file "${skips}")
   fi
 
-  echo "./run-test-plan.py ${cmd_args[*]} ${PLAN_NAME}${variants}:${modules} ${config}"
-  ./run-test-plan.py "${cmd_args[@]}" "${PLAN_NAME}${variants}:${modules}" "${config}"
+  echo "./run-test-plan.py ${cmd_args[*]} ${plan}${variants}:${modules} ${config}"
+  ./run-test-plan.py "${cmd_args[@]}" "${plan}${variants}:${modules}" "${config}"
 }
 
 
@@ -190,32 +240,51 @@ clean_plans() {
 # Run test modules
 #
 run_modules() {
-  local modules="$1"
-  local config="$2"
+  local role="$1"
+  local modules="$2"
+  local config="$3"
 
-  local failures="${DEFAULT_FAILURES_FILE}"
-  local skips="${DEFAULT_SKIPS_FILE}"
+  plan=$(_get_plan_name "${role}")
+  variants=$(jq -r ".${role}.variants" <<< "${SCRIPT_CONFIG}")
+  failures="${SCRIPT_DIR}/config/$(jq -r ".${role}.failures_file" <<< "${SCRIPT_CONFIG}")"
+  skips="${SCRIPT_DIR}/config/$(jq -r ".${role}.skips_file" <<< "${SCRIPT_CONFIG}")"
 
   if [[ -z "${modules}" ]]; then
-    modules=$(printf "%s\n" "$(_get_effective_modules)" | paste -sd "," -)
+    modules=$(printf "%s\n" "$(_get_effective_modules ${role})" | paste -sd "," -)
 
-    echo "Filtered modules for test plan ${PLAN_NAME}"
-    for mod in $(_get_filtered_modules); do
-      printf " - %s\n" "$mod"
-    done
+    filtered_modules=$(_get_filtered_modules "${role}")
+    if [ -n "$filtered_modules" ]; then
+      echo "Filtered modules for test plan ${plan}"
+      for mod in $filtered_modules; do
+        printf " - %s\n" "$mod"
+      done
+    fi
   else
     failures=""
     skips=""
   fi
 
-  _run_test_modules "${DEFAULT_VARIANTS}" "${modules}" "${failures}" "${skips}" "${config}"
+  if [ -n "${modules}" ]; then
+    _run_test_modules "${role}" "${plan}" "${variants}" "${modules}" "${failures}" "${skips}" "${config}"
+  fi
 }
 
-# Run the default profile
+# Run the default profile for the given role
 #
-run_default_profile() {
-  echo "Run profile: default";
-  run_modules "" "${DEFAULT_CONFIG_FILE}"
+run_profile() {
+  local role="$1"
+  echo "Run profile: ${role}";
+  config="${SCRIPT_DIR}/config/$(jq -r ".${role}.config_file" <<< "${SCRIPT_CONFIG}")"
+  run_modules "${role}" "" "${config}"
+}
+
+# Run the profile verifier
+#
+run_profile_verifier() {
+  role="verifier"
+  echo "Run profile: ${role}";
+  config="${SCRIPT_DIR}/config/$(jq -r ".${role}.config_file" <<< "${SCRIPT_CONFIG}")"
+  run_modules "${role}" "" "${config}"
 }
 
 # Run a profile 'oid4vci-attestation-proof'
@@ -223,13 +292,17 @@ run_default_profile() {
 run_profile_oid4vci_attestation_proof() {
   echo "Run profile: oid4vci-attestation-proof";
 
+  role="issuer"
+  plan=$(_get_plan_name "${role}")
+  variants=$(jq -r ".${role}.variants" <<< "${SCRIPT_CONFIG}")
   modules="oid4vci-1_0-issuer-fail-invalid-key-attestation-signature"
-  config="${SCRIPT_DIR}/config/.keycloak-openid-config-oid4vci-attestation-proof.json"
+  config_in="${SCRIPT_DIR}/config/$(jq -r ".${role}.config_file" <<< "${SCRIPT_CONFIG}")"
+  config_out="${SCRIPT_DIR}/config/.keycloak-openid-config-oid4vci-attestation-proof.json"
 
   # Transform the config
-  jq '.vci.credential_proof_type_hint = "attestation"' "${DEFAULT_CONFIG_FILE}" > "${config}"
+  jq '.vci.credential_proof_type_hint = "attestation"' "${config_in}" > "${config_out}"
 
-  _run_test_modules "${DEFAULT_VARIANTS}" "${modules}" "" "" "${config}"
+  _run_test_modules "${role}" "${plan}" "${variants}" "${modules}" "" "" "${config_out}"
 }
 
 # Run a profile 'oid4vci-credential-encryption'
@@ -237,8 +310,13 @@ run_profile_oid4vci_attestation_proof() {
 run_profile_oid4vci_credential_encryption() {
   echo "Run profile: oid4vci-credential-encryption";
 
+  role="issuer"
+  plan=$(_get_plan_name "${role}")
+  variants=$(jq -r ".${role}.variants" <<< "${SCRIPT_CONFIG}")
   modules="oid4vci-1_0-issuer-happy-flow,oid4vci-1_0-issuer-fail-unknown-credential-configuration"
-  _run_test_modules "${DEFAULT_VARIANTS}" "${modules}" "" "" "${DEFAULT_CONFIG_FILE}"
+  config="${SCRIPT_DIR}/config/$(jq -r ".${role}.config_file" <<< "${SCRIPT_CONFIG}")"
+
+  _run_test_modules "${role}" "${plan}" "${variants}" "${modules}" "" "" "${config}"
 }
 
 # Run a profile 'fapi2-user-rejects-authentication'
@@ -256,8 +334,12 @@ run_profile_fapi2_user_rejects_authentication() {
   kcadm update "clients/${cid2}" -r ${KC_REALM} -s consentRequired=true
   echo "${KC_CLIENT2} ${cid2} consentRequired=true"
 
+  role="issuer"
+  plan=$(_get_plan_name "${role}")
+  variants=$(jq -r ".${role}.variants" <<< "${SCRIPT_CONFIG}")
   modules="fapi2-security-profile-final-user-rejects-authentication"
-  config="${SCRIPT_DIR}/config/.keycloak-openid-config-fapi2-user-rejects-authentication.json"
+  config_in="${SCRIPT_DIR}/config/$(jq -r ".${role}.config_file" <<< "${SCRIPT_CONFIG}")"
+  config_out="${SCRIPT_DIR}/config/.keycloak-openid-config-fapi2-user-rejects-authentication.json"
 
   jq '.browser[0].tasks |=
     (.[:1] + [{
@@ -266,9 +348,9 @@ run_profile_fapi2_user_rejects_authentication() {
       "commands": [
         ["click", "id", "kc-cancel"]
       ]
-    }] + .[1:])' "${DEFAULT_CONFIG_FILE}" > "${config}"
+    }] + .[1:])' "${config_in}" > "${config_out}"
 
-  _run_test_modules "${DEFAULT_VARIANTS}" "${modules}" "" "" "${config}"
+  _run_test_modules "${role}" "${plan}" "${variants}" "${modules}" "" "" "${config_out}"
 
   kcadm update "clients/${cid1}" -r ${KC_REALM} -s consentRequired=false
   kcadm update "clients/${cid2}" -r ${KC_REALM} -s consentRequired=false
@@ -277,18 +359,23 @@ run_profile_fapi2_user_rejects_authentication() {
 # Show effective test modules
 #
 show_modules() {
-  effective_modules=$(_get_effective_modules)
-  filtered_modules=$(_get_filtered_modules)
+  local role="$1"
+  
+  plan=$(_get_plan_name "${role}")
+  effective_modules=$(_get_effective_modules "${role}")
+  filtered_modules=$(_get_filtered_modules "${role}")
 
-  echo "Modules for test plan ${PLAN_NAME}"
+  echo "Modules for test plan ${plan}"
   for mod in ${effective_modules}; do
     printf " - %s\n" "$mod"
   done
 
-  echo "Filtered modules for test plan ${PLAN_NAME}"
-  for mod in ${filtered_modules}; do
-    printf " - %s\n" "$mod"
-  done
+  if [ -n "$filtered_modules" ]; then
+    echo "Filtered modules for test plan ${plan}"
+    for mod in $filtered_modules; do
+      printf " - %s\n" "$mod"
+    done
+  fi
 }
 
 
@@ -298,31 +385,46 @@ if [[ ${opt_clean} == true ]]; then
   clean_plans
 fi
 
-# Optionally show pre-configured modules for the given test plan -------------------------------------------------------
+# Show pre-configured modules for the given role -----------------------------------------------------------------------
 #
-if [[ ${opt_show_modules} == true ]]; then
+if [[ -n ${opt_show_role} ]]; then
   _activate_venv
-  show_modules
+  case "${opt_show_role}" in
+    issuer)
+      show_modules "${opt_show_role}"
+      ;;
+    verifier)
+      show_modules "${opt_show_role}"
+      ;;
+  esac
   _deactivate_venv
   exit 0
 fi
 
 # Run all pre-configured modules for the given test plan ---------------------------------------------------------------
 #
-if [[ ${opt_run_all} == true ]]; then
+if [[ -n "${opt_run_role}" && -z ${opt_run_module} ]]; then
   _activate_venv
-  run_profile_fapi2_user_rejects_authentication
-  run_profile_oid4vci_attestation_proof
-  run_default_profile
+  case "${opt_run_role}" in
+    issuer)
+      run_profile_fapi2_user_rejects_authentication
+      run_profile_oid4vci_attestation_proof
+      run_profile_issuer
+      ;;
+    verifier)
+      run_profile "${opt_run_role}"
+      ;;
+  esac
   _deactivate_venv
   exit 0
 fi
 
 # Run a single module from the given test plan -------------------------------------------------------------------------
 #
-if [[ -n ${opt_run_module} ]]; then
+if [[ -n ${opt_run_role} && -n ${opt_run_module} ]]; then
   _activate_venv
-  run_modules "${opt_run_module}" "${opt_run_config:-$DEFAULT_CONFIG_FILE}"
+  default_config="${SCRIPT_DIR}/config/$(jq -r ".${opt_run_role}.config_file" <<< "${SCRIPT_CONFIG}")"
+  run_modules "${opt_run_role}" "${opt_run_module}" "${opt_run_config:-$default_config}"
   _deactivate_venv
   exit 0
 fi
@@ -331,17 +433,20 @@ fi
 #
 if [[ -n ${opt_run_profile} ]]; then
   _activate_venv
-  case "$opt_run_profile" in
-    0|default)
-      run_default_profile
+  case "${opt_run_profile}" in
+    1|issuer)
+      run_profile "issuer"
       ;;
-    1|oid4vci-attestation-proof)
+    2|verifier)
+      run_profile "verifier"
+      ;;
+    3|oid4vci-attestation-proof)
       run_profile_oid4vci_attestation_proof
       ;;
-    2|oid4vci-credential-encryption)
+    4|oid4vci-credential-encryption)
       run_profile_oid4vci_credential_encryption
       ;;
-    3|fapi2-user-rejects-authentication)
+    5|fapi2-user-rejects-authentication)
       run_profile_fapi2_user_rejects_authentication
       ;;
     *)
